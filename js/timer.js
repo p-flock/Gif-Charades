cf978be8a53c7f0a679132ca42681a0777670b71
Wilmer Paulino <wilmer.paulino@gmail.com>
2015-03-08 04:19:03 -0400

    fixed variable names

--- js/timer.js
+++ js/timer.js
@@ -2,12 +2,12 @@ var seconds = 60;                           // Countdown time
 var countdown = setInterval(timer, 1000);   // Set timer function to run every second
 
 function timer() {
-    count = count - 1;
+    seconds = seconds - 1;
     
-    if (counter <= 0) {
+    if (countdown <= 0) {
         // do something
         return;
     }
 
-    document.getElementById("timer").innerHTML = count;
+    document.getElementById("timer").innerHTML = seconds;
 }
