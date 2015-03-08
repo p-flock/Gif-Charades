72d9948417aa3d44976492e87c2a29875e218fc7
p-flock <peter.flockhart@gmail.com>
2015-03-07 19:48:57 -0500

    added auto resizing with proper aspect ratios for gifs

--- js/gif.js
+++ js/gif.js
@@ -8,7 +8,8 @@ $(document).ready(function() {
             temp = response;
             console.log("success got data", response);
             random = getRandom();
-            show_image(response.data[random].images.original.url, "Guess the word!");
+            //var select_gif = response.data[random].images.original;
+            show_image(response.data[random].images.url, response.data[random].images.height, response.data[random].images.width, "Guess the word!");
         });
     });
 });
@@ -17,16 +18,22 @@ function getRandom() {
     return Math.floor((Math.random() * 25));
 }
 
-function show_image(src, alt) {
+function show_image(src, height, width, alt) {
+    var resized = resize(height, width);
+    height = resized[0];
+    width = resized[1];
+
     var img = document.createElement("img");
     img.src = src;
-    img.width = temp.data[random].images.original.width;
-    img.height = temp.data[random].images.original.height;
+    img.width = width; //temp.data[random].images.original.width;
+    img.height = height; //temp.data[random].images.original.height;
     img.alt = alt;
     document.body.appendChild(img);
 }
 
 function resize(height, width) {
+    var MAX_HEIGHT = 400;
+    var MAX_WIDTH = 400;
     var gcd = function(heigh, width) {
         if ( ! b) {
                 return a;
@@ -36,6 +43,10 @@ function resize(height, width) {
     };
     width = width / gcd;
     height = height / gcd;
-
-
+    var width_mult = MAX_WIDTH / width;
+    var height_mult = MAX_HEIGHT / height;
+    var multiplier = Math.min(width_mult, height_mult);
+    width = width * multiplier;
+    height = height * multiplier;
+    return [height, width];
 }
