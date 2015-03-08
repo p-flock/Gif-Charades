8987c7ab802618dd99498fd195a488e2ada5fb20
p-flock <peter.flockhart@gmail.com>
2015-03-07 19:16:21 -0500

    Added resizing function to be completed later

--- js/gif.js
+++ js/gif.js
@@ -1,10 +1,17 @@
-function getGifs() {
-    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+document.getElementById("search_tag").value+"&api_key=dc6zaTOxFJmzC");
-    xhr.done(function(response) { 
-        console.log("success got data", response);
-        show_image(response.data[getRandom()].images.original.url, "Guess the word!");
+var temp;
+var random;
+$(document).ready(function() {
+    $("button").click(function() {
+        var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+document.getElementById("search_tag").value+"&api_key=dc6zaTOxFJmzC");
+        xhr.done(
+            function(response) { 
+            temp = response;
+            console.log("success got data", response);
+            random = getRandom();
+            show_image(response.data[random].images.original.url, "Guess the word!");
+        });
     });
-}
+});
 
 function getRandom() {
     return Math.floor((Math.random() * 25));
@@ -13,8 +20,22 @@ function getRandom() {
 function show_image(src, alt) {
     var img = document.createElement("img");
     img.src = src;
-    img.width = 300;
-    img.height = 300;
+    img.width = temp.data[random].images.original.width;
+    img.height = temp.data[random].images.original.height;
     img.alt = alt;
     document.body.appendChild(img);
+}
+
+function resize(height, width) {
+    var gcd = function(heigh, width) {
+        if ( ! b) {
+                return a;
+            }
+    
+        return gcd(b, a % b);
+    };
+    width = width / gcd;
+    height = height / gcd;
+
+
 }
