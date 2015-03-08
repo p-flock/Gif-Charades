a862a17fde9842150718515a0402a58414b1e849
p-flock <peter.flockhart@gmail.com>
2015-03-07 23:02:56 -0500

    combined .js files into one, gif.js

--- js/gif.js
+++ js/gif.js
@@ -1,6 +1,9 @@
 var temp;
 var random;
 var img;
+
+// This function is called when the user submits an answer
+// 
 $(document).ready(function() {
     $("button").click(function() {
         var user = document.getElementById("search_tag").value;
@@ -23,21 +26,25 @@ $(document).ready(function() {
     });
 });
 
+
 function clearFields(){
     document.getElementById("search_tag").value = "";
     document.getElementById("gif-place").removeChild(img);
 }
 
+
 function newQuestion(){
     $('#question').html(function() { return "i'm testing" });
 }
+
+
 function getRandom() {
     return Math.floor((Math.random() * 25));
 }
 
 
-function display_image(){
-    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=roses&api_key=dc6zaTOxFJmzC");
+function display_image(search_term){
+    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + search_term + "&api_key=dc6zaTOxFJmzC");
         xhr.done(
             function(response) {
             temp = response;
@@ -47,6 +54,7 @@ function display_image(){
             show_image(response.data[random].images.original.url, response.data[random].images.original.height, response.data[random].images.original.width, "Guess the word!");
         });
 }
+
 
 function show_image(src, height, width, alt) {
     console.log(height, width);
@@ -62,6 +70,7 @@ function show_image(src, height, width, alt) {
     img.alt = alt;
     document.getElementById("gif-place").appendChild(img);
 }
+
 
 function resize(height, width) {
 
@@ -86,10 +95,77 @@ function resize(height, width) {
     return [height, width];
 }
 
+
 function get_gcd (a, b) {
     if ( ! b) {
         return a;
     }
     return get_gcd(b, a % b);
 };
+
+
+/*
+ *  THIS IS FOR SPACE 
+ *
+ */
+
+var sentences = {
+    "Roses are red": [0],
+    "The cat ate an apple":[1, 4],
+    "The dog ate the cat's food":[1, 4, 5]
+}
+
+/* selects a random sentence from the remaining possible sentences
+ * and returns an array containing both the sentence as a string
+ * and the indices of the words that should be converted to gifs
+ */ 
+function getRandomSentence(possible_sentences) {
+
+    var key = getRandomKey(possible_sentences);
+    var value = possible_sentences[key];
+    // remove used key so we don't repeat puzzles (TODO)
+    return [key, value];
+}
+
+
+function getRandomKey (sentences) {
+    var result;
+    var count = 0;
+    for (var prop in sentences)
+        if (Math.random() < 1/++count)
+           result = prop;
+    return result;
+}
+
+
+function display_puzzle () {
+    /* gets random sentence
+     * iterates through sentence:
+     * if word is regular word: put it in body of html
+     * if word should be gif:
+     *          convert to gif, display in body of html
+     *
+     * create appropriate text boxes for answers
+     * returns answers to puzzle, to be checked by other function
+     */
+    
+    // sentence will be an array containing a string and an array of integers
+    var sentence = getRandomSentence(sentences);
+    var words = sentence[0].split(" ");
+
+    var answers = [];
+    for (index in sentence[1]) {
+        answers.append(words[index]);
+    }
+
+    var toDisplay = [];
+    for(word in words) {
+        if (word in answers)
+            toDisplay.append(convertToGif(word));
+        else
+            toDisplay.append(word);
+            
+    }
+}
+
 
---
+++ js/sentence_list.js
@@ -0,0 +1,6 @@
+var sentences = {
+    "Roses are red": [0],
+    "The cat ate an apple":[1, 4],
+    "The dog ate the cat's food":[1, 4, 5]
+
+}
